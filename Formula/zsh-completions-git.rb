2e5dbc5ead797049dcb612e0425232ea3314fbd9
univerio <1037172+univerio@users.noreply.github.com>
2022-09-10 01:10:38 +0000

    zsh-completions-git: add 0.0.1 bottle.

--- Formula/zsh-completions-git.rb
+++ Formula/zsh-completions-git.rb
@@ -5,6 +5,12 @@ class ZshCompletionsGit < Formula
   version "0.0.1"
   sha256 "fb930fdaf2e3125ca23d5e3d706b4c6ceecd88923b936b5234f04e31f7cbd23f"
   license :public_domain
+
+  bottle do
+    root_url "https://github.com/univerio/homebrew-zsh-completions-git/releases/download/zsh-completions-git-0.0.1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "86e4c5c474be81b272ef5959693de0b939336dd9b779dc422a3659a84ff715a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5131adceb9289e188ccd1770399f78c8d1b9ab9d27bf99f8fd9e785687557ffb"
+  end
   depends_on "zsh"
 
   def share_zsh_completions
